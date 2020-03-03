44b59b7d94bbe195adfc8157abcba99bf813a2e6
Manuel Lauterbach <ma.lauterbach@gmail.com>
2020-03-03 20:24:31 +0100

    - implemented pair - implemented a lot of type traits

--- astd/src/type_traits.h
+++ astd/src/type_traits.h
@@ -72,6 +72,9 @@ namespace astd
     struct conjunction<T, Ts...> : conditional_t<bool(T::value), conjunction<Ts...>, T>
     {};
 
+    template<typename T, typename... Ts>
+    constexpr auto conjuction_v = conjunction<T, Ts...>::value;
+
     template<typename T, typename = void>
     struct add_lvalue_reference_impl
     {
@@ -132,22 +135,54 @@ namespace astd
     constexpr auto is_constructible_v = is_constructible<T, Args...>::value;
 
     template<typename T>
-    using is_default_constructible = is_constructible_impl<void_t<>, T>;
+    using is_default_constructible = is_constructible<T>;
 
     template<typename T>
     constexpr auto is_default_constructible_v = is_default_constructible<T>::value;
 
     template<typename T>
-    using is_copy_constructible = is_constructible_impl<void_t<>, T, add_lvalue_reference_t<const T>>;
+    using is_copy_constructible = is_constructible<T, add_lvalue_reference_t<const T>>;
 
     template<typename T>
     constexpr auto is_copy_constructible_v = is_copy_constructible<T>::value;
+
+    template<typename T>
+    using is_move_constructible = is_constructible<T, T>;
+
+    template<typename T>
+    constexpr auto is_move_constructible_v = is_move_constructible<T>::value;
+
+    template<typename, typename T, typename U>
+    struct is_assignable_impl : false_type
+    {};
+
+    template<typename T, typename U>
+    struct is_assignable_impl<void_t<decltype(declval<T>() = declval<U>())>, T, U> : true_type
+    {};
+
+    template<typename T, typename U>
+    using is_assignable = is_assignable_impl<void_t<>, T, U>;
+
+    template<typename T, typename U>
+    constexpr auto is_assignable_v = is_assignable<T, U>::value;
+
+    template<typename T>
+    using is_copy_assignable = is_assignable<add_lvalue_reference_t<T>, add_lvalue_reference_t<const T>>;
+
+    template<typename T>
+    constexpr auto is_copy_assignable_v = is_copy_assignable<T>::value;
+
+    template<typename T>
+    using is_move_assignable = is_assignable<add_lvalue_reference_t<T>, T>;
+
+    template<typename T>
+    constexpr auto is_move_assignable_v = is_move_assignable<T>::value;
 
     template<typename, typename From, typename To>
     struct is_convertible_impl : false_type
     {};
 
-    // I am not sure this is standard conform but this will have to do for the moment
+    // I am not sure if this is standard conform, but this will have to do for the moment
     template<typename From, typename To>
     struct is_convertible_impl<void_t<decltype((To)declval<From>())>, From, To> : true_type
     {};
@@ -166,6 +201,9 @@ namespace astd
     struct is_same<T, T> : true_type
     {};
 
+    template<typename T, typename U>
+    constexpr auto is_same_v = is_same<T, U>::value;
+
     template<typename T>
     struct is_array : false_type
     {};
@@ -177,6 +215,99 @@ namespace astd
     template<typename T, size_t _size>
     struct is_array<T[_size]> : true_type
     {};
+
+    template<typename T>
+    constexpr auto is_array_v = is_array<T>::value;
+
+    template<typename T>
+    struct is_const : false_type
+    {};
+
+    template<typename T>
+    struct is_const<const T> : true_type
+    {};
+
+    template<typename T>
+    constexpr auto is_const_v = is_const<T>::value;
+
+    template<typename T>
+    struct is_reference : false_type
+    {};
+
+    template<typename T>
+    struct is_reference<T&> : true_type
+    {};
+
+    template<typename T>
+    struct is_reference<T&&> : true_type
+    {};
+
+    template<typename T>
+    constexpr auto is_reference_v = is_reference<T>::value;
+
+    template<typename T>
+    struct is_function : bool_constant<!is_const_v<const T> && !is_reference_v<T>>
+    {};
+
+    template<typename T>
+    constexpr auto is_function_v = is_function<T>::value;
+
+    template<typename T>
+    struct remove_const
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_const<const T>
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    using remove_const_t = typename remove_const<T>::type;
+
+    template<typename T>
+    struct remove_volatile
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_volatile<volatile T>
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    using remove_volatile_t = typename remove_volatile<T>::type;
+
+    template<typename T>
+    struct remove_cv
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_cv<const T>
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_cv<volatile T>
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_cv<const volatile T>
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    using remove_cv_t = typename remove_cv<T>::type;
 
     template<typename T>
     struct remove_reference
@@ -198,6 +329,59 @@ namespace astd
 
     template<typename T>
     using remove_reference_t = typename remove_reference<T>::type;
+
+    template<typename T>
+    struct remove_extent
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_extent<T[]>
+    {
+        using type = T;
+    };
+
+    template<typename T, size_t _size>
+    struct remove_extent<T[_size]>
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    using remove_extent_t = typename remove_extent<T>::type;
+
+    template<typename T, typename = void>
+    struct add_pointer_impl
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    struct add_pointer_impl<T, void_t<remove_reference_t<T>*>>
+    {
+        using type = remove_reference_t<T>*;
+    };
+
+    template<typename T>
+    struct add_pointer
+    {
+        using type = typename add_pointer_impl<T>::type;
+    };
+
+    template<typename T>
+    using add_pointer_t = typename add_pointer<T>::type;
+
+    template<typename T>
+    struct decay
+    {
+        using type = conditional_t<is_function_v<remove_reference_t<T>>, add_pointer_t<remove_reference_t<T>>,
+            conditional_t<is_array_v<remove_reference_t<T>>, add_pointer_t<remove_extent_t<remove_reference_t<T>>>,
+            remove_cv_t<remove_reference_t<T>>>>;
+    };
+
+    template<typename T>
+    using decay_t = typename decay<T>::type;
 
     template<typename T>
     constexpr T&& forward(typename remove_reference<T>::type& t)
---
+++ astd/src/utility.h
@@ -0,0 +1,144 @@
+#pragma once
+#ifndef ASTD_UTILITY
+#define ASTD_UTILITY
+
+#include "type_traits.h"
+
+namespace astd
+{
+    struct _noCopyMove_t
+    {
+        ~_noCopyMove_t() = delete;
+        _noCopyMove_t(_noCopyMove_t const&) = delete;
+        void operator=(_noCopyMove_t const&) = delete;
+    };
+
+    /* TODOs pair
+        swap, get, refactor make_pair when reference_wrapper is implemented
+    */
+
+
+    template<typename T1, typename T2>
+    struct pair
+    {
+        using first_type = T1;
+        using second_type = T2;
+
+        // TODO explicit overload
+        template<typename U1 = T1, typename U2 = T2, enable_if_t<conjunction<is_default_constructible<U1>, is_default_constructible<U2>>::value, int> = 0>
+        constexpr pair()
+            : first(), second()
+        {}
+
+        // TODO explicit overload
+        template<typename U1 = T2, typename U2 = T2, enable_if_t<conjunction<is_copy_constructible<U1>, is_copy_constructible<U2>>::value, int> = 0>
+        pair(const T1& t1, const T2& t2)
+            : first(t1), second(t2)
+        {}
+
+        // TODO explicit overload
+        template<typename U1, typename U2, enable_if_t<conjunction<is_constructible<T1, U1&&>, is_constructible<T2, U2&&>>::value, int> = 0>
+        pair(U1&& u1, U2&& u2)
+            : first(forward<U1>(u1)), second(forward<U2>(u2))
+        {}
+
+        // TODO explicit overload
+        template<typename U1, typename U2, enable_if_t<conjunction<is_constructible<T1, const U1&>, is_constructible<T1, const T2&>>::value, int> = 0>
+        constexpr pair(const pair<U1, U2>& other)
+            : first(other.first), second(other.second)
+        {}
+
+        // TODO explicit overload
+        template<typename U1, typename U2, enable_if_t<conjunction<is_constructible<T1, U1&&>, is_constructible<T2, U2&&>>::value, int> = 0>
+        constexpr pair(pair<U1, U2>&& other)
+            : first(forward<U1>(other.first)), second(forward<U2>(other.second))
+        {}
+
+        // TODO piecewise construct overload
+
+        constexpr pair(const pair& other) = default;
+        constexpr pair(pair&& other) = default;
+
+        pair& operator=(typename conditional<conjunction<is_copy_assignable<T1>, is_copy_assignable<T2>>::value,
+            const pair&, _noCopyMove_t&>::type other)
+        {
+            first = other.first;
+            second = other.second;
+            return *this;
+        }
+
+        template<typename U1, typename U2, enable_if_t<conjunction<is_assignable<T1&, const U1&>, is_assignable<T2&, const U2&>>::value, int> = 0>
+        constexpr pair& operator=(const pair<U1, U2>& other)
+        {
+            first = other.first;
+            second = other.second;
+            return *this;
+        }
+
+        // TODO noexcept with is_nothrow_move_assignable
+        pair& operator=(typename conditional<conjunction<is_move_assignable<T1>, is_move_assignable<T2>>::value,
+            pair&&, _noCopyMove_t&&>::type other) noexcept
+        {
+            first = forward<T1>(other.first);
+            second = forward<T2>(other.second);
+            return *this;
+        }
+
+        // TODO noexcept with is_nothrow_assignable
+        template<typename U1, typename U2, enable_if_t<conjunction<is_assignable<T1&, U1&&>, is_assignable<T2&, U2&&>>::value, int> = 0>
+        constexpr pair& operator=(pair<U1, U2>&& other)
+        {
+            first = forward<U1>(other.first);
+            second = forward<U2>(other.second);
+            return *this;
+        }
+
+        T1 first;
+        T2 second;
+    };
+
+    template<typename T1, typename T2>
+    [[nodiscard]] constexpr bool operator==(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+    {
+        return lhs.first == rhs.first && lhs.second == rhs.second;
+    }
+
+    template<typename T1, typename T2>
+    [[nodiscard]] constexpr bool operator!=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+    {
+        return !(lhs == rhs);
+    }
+
+    template<typename T1, typename T2>
+    [[nodiscard]] constexpr bool operator<(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+    {
+        return lhs.first < rhs.first || (!(rhs.first < lhs.first) && lhs.second < rhs.second);
+    }
+
+    template<typename T1, typename T2>
+    [[nodiscard]] constexpr bool operator>(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+    {
+        return rhs < lhs;
+    }
+
+    template<typename T1, typename T2>
+    [[nodiscard]] constexpr bool operator<=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+    {
+        return !(rhs < lhs);
+    }
+
+    template<typename T1, typename T2>
+    [[nodiscard]] constexpr bool operator>=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+    {
+        return !(lhs < rhs);
+    }
+
+    // TODO, refactor when reference_wrapper is implemented
+    template<typename T1, typename T2>
+    pair<decay_t<T1>, decay_t<T2>> make_pair(T1&& t1, T2&& t2)
+    {
+        return pair<decay_t<T1>, decay_t<T2>>(forward<T1>(t1), forward<T2>(t2));
+    }
+}
+
+#endif // ASTD_UTILITY
